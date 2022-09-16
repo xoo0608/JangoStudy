c9400f12e3272bc5f77b4f3bb5e0e4ca839e7604
xoo0608 <xoo0608@naver.com>
2022-09-16 22:25:28 +0900

    data update

--- mysite/chantest.py
+++ mysite/chantest.py
@@ -1,23 +1,28 @@
 import json
+from xmlrpc.server import DocCGIXMLRPCRequestHandler
 from bs4 import BeautifulSoup   
 import requests
 from urllib.request import urlopen
 import config
+import time
+from datetime import datetime, timedelta
+
 
 def savedata(date):
         data = get_obj(date)
-        file_path = "./data/" + date + ".json"
-
-        with open(file_path, 'w', encoding='utf-8') as file:
-                json.dump(data, file)
+        file_path = "./2022data/" + date + ".json"
+        datalist = data.get("response").get("body").get("items").get("item")
+        if len(datalist) > 1:
+                print("no!")
+                with open(file_path, 'w', encoding='utf-8') as file:
+                        json.dump(data, file)
 
 def get_obj(date):
-    url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey="+ config.APT_KEY +"&numOfRows=10000&resultType=json&basDt=" + date
-    result = requests.get(url,verify=False).text
-    data = json.loads(result)
-    datalist = data.get("response").get("body").get("items").get("item")
-    print(len(datalist))
-    return data
+        time.sleep(0.3)
+        url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey="+ config.APT_KEY +"&numOfRows=10000&resultType=json&basDt=" + date
+        result = requests.get(url,verify=False).text
+        data = json.loads(result)
+        return data
 
 def get_json(date):
         with open ("data/" + date + ".json", "r") as f:
@@ -28,6 +33,22 @@ def get_json(date):
 
 #get_json("20220908")
 #get_obj("20220908")
-savedata("20220914")
+savedata("20220916")
 
 
+# start = "20211213"
+# last = "20211231"
+
+# # 시작일, 종료일 datetime 으로 변환
+# start_date = datetime.strptime(start, "%Y%m%d")
+# last_date = datetime.strptime(last, "%Y%m%d")
+
+
+
+# 종료일 까지 반복
+# while start_date <= last_date:
+#     dates = start_date.strftime("%Y%m%d")
+#     # print(dates)
+#     savedata(str(dates))
+#     # 하루 더하기
+#     start_date += timedelta(days=1)
